28da6690821ef6a2de404c84c70c78e16b172f3a
pc <pc@DESKTOP-78M62IA>
2024-10-24 21:38:59 +0300

    Add project files.

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Lütfen bir sayı girin: ");
+
+        try
+        {
+            string girdi = Console.ReadLine();
+            double sayi = Convert.ToDouble(girdi);
+            double kare = sayi * sayi;
+            Console.WriteLine($"Girdiğiniz sayının karesi: {kare}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen daha küçük bir sayı giriniz.");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Bir hata oluştu! Lütfen geçerli bir giriş yapın.");
+        }
+    }
+}
